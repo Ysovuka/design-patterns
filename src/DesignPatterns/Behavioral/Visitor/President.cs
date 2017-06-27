b5c63e8ddad09138f471b18e40cd9e5d99800c52
Joshua Thompson <ysovuka@gmail.com>
2017-06-26 21:20:14 -0500

    Moved the Visitor design pattern into the new project.

---
+++ src/DesignPatterns/Behavioral/Visitor/President.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Behavioral.Visitor
+{
+    public class President : Employee
+    {
+        public President(string name, double income, int vacationDays)
+            : base(name, income, vacationDays) { }
+    }
+}
